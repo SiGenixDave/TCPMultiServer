7e778c6332e507c81503814134fca10484719d76
Dave <dsmail@sigenix.com>
2014-05-28 15:34:30 -0400

    Initial Commit of Dave Hello World code

---
+++ src/DaveHelloWorld.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : DaveHelloWorld.c
+ Author      : DAS
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
